5be2746de16ef1c125a191983f8ce48003e6f134
DpaHbaLT <47538815+DpaHbaLT@users.noreply.github.com>
2024-05-11 12:43:10 +0300

    Update jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,1 +1,20 @@
-pipeline/jenkins.groovy 
+pipeline {
+    agent any
+    parameters {
+
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+        choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick arch')
+        
+
+    }
+    stages {
+        stage('Example') {
+            steps {
+                echo "Build for platform ${params.OS}"
+
+                echo "Build for arch: ${params.ARCH}"
+
+            }
+        }
+    }
+}
